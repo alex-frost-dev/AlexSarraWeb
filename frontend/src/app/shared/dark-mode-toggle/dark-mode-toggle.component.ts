240ef1a3496b9242f204ab90e962de575e44ffb9
A50tt <alexsarraolmo@gmail.com>
2025-10-01 01:49:34 +0200

    darkmode toggle completed!!

--- frontend/src/app/shared/dark-mode-toggle/dark-mode-toggle.component.ts
+++ frontend/src/app/shared/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DarkModeService } from '../services/dark-mode/dark-mode.service';
 import { CommonModule } from '@angular/common';
 
@@ -10,7 +10,63 @@ import { CommonModule } from '@angular/common';
   standalone: true,
 })
 export class DarkModeToggleComponent {
+  @Input() width!: number;
+  @Input() height!: number;
+  prefGap!: number;
+
+  toggleElem!: HTMLImageElement;
+  parentContainer!: HTMLDivElement;
+  container!: HTMLDivElement;
+  leftElem!: HTMLImageElement;
+  rightElem!: HTMLImageElement;
+
   constructor(public darkModeService: DarkModeService) {}
 
-  toggleDarkMode() {}
+  ngOnInit() {
+    // Elements selection
+    this.toggleElem = document.querySelector(
+      '.toggle-switch',
+    ) as HTMLImageElement;
+    this.parentContainer = document.querySelector(
+      '.parent-toggle-container',
+    ) as HTMLDivElement;
+    this.container = document.querySelector(
+      '.toggle-container',
+    ) as HTMLDivElement;
+    this.leftElem = document.querySelector('.left-image') as HTMLImageElement;
+    this.rightElem = document.querySelector('.right-image') as HTMLImageElement;
+
+    // Gap calculation
+    this.prefGap = this.width - this.height * 2;
+
+    this.parentContainer.style.height = this.height + 'px';
+    this.container.style.height = this.height + 'px';
+    this.toggleElem.style.height = this.height + 'px';
+
+    this.container.style.gap = this.prefGap + 'px';
+    this.leftElem.style.height = this.height + 'px';
+    this.leftElem.style.width = this.height + 'px'; // <---
+    this.rightElem.style.height = this.height + 'px';
+    this.rightElem.style.width = this.height + 'px';
+  }
+
+  adjustToggle() {
+    let toggleWidth = parseInt(
+      window.getComputedStyle(this.toggleElem).width.replace('px', ''),
+    );
+
+    let containerWidth = parseInt(
+      window.getComputedStyle(this.container).width.replace('px', ''),
+    );
+
+    var pixels = containerWidth - toggleWidth;
+
+    if (this.darkModeService.isDarkMode()) {
+      // Slide left
+      this.toggleElem.style.transform = 'translateX(0px)';
+    } else {
+      // Slide right
+      this.toggleElem.style.transform = 'translateX(' + pixels + 'px)';
+    }
+  }
 }
