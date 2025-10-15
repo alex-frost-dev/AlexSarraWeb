7c371e36cb5741194385ccb89a496dc9577d26fb
A50tt <alexsarraolmo@gmail.com>
2025-10-15 17:49:53 +0200

    pdf and site deleted

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
-import { MaintenanceLayoutComponent } from './core/layouts/maintenance-layout/maintenance-layout.component';
-import { PageNotFoundLayoutComponent } from './core/layouts/page-not-found-layout/page-not-found-layout.component';
-import { MaintenanceModule } from './maintenance/maintenance.module';
-import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 
 export const routes: Routes = [
   {
